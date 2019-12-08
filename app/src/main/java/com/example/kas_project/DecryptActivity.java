ecb2d727b42a9b2a2512c962ac7dd215e2c63ca2
xkopri10 <xkopri10@node.mendelu.cz>
2019-12-09 00:23:48 +0100

    ui

---
+++ app/src/main/java/com/example/kas_project/DecryptActivity.java
@@ -0,0 +1,30 @@
+package com.example.kas_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.Objects;
+
+public class DecryptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_decrypt);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int idItem = item.getItemId();
+        if (idItem == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/kas_project/MainActivity.java
+++ app/src/main/java/com/example/kas_project/MainActivity.java
@@ -2,13 +2,72 @@ package com.example.kas_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button generateKeysButton, decryptButton, encryptButton, aboutAppButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        generateKeysButton = findViewById(R.id.generate_and_send_button);
+        decryptButton = findViewById(R.id.decrypt_button);
+        encryptButton = findViewById(R.id.encrypt_button);
+        aboutAppButton = findViewById(R.id.about_app_button);
+
+        generateKeysButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGenerateKeyAndSendKeyActivity();
+            }
+        });
+
+        decryptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDecryptActivity();
+            }
+        });
+
+        encryptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openEncryptActivity();
+            }
+        });
+
+        aboutAppButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAboutAppActivity();
+            }
+        });
+
+    }
+
+    private void openGenerateKeyAndSendKeyActivity() {
+        Intent intent = new Intent(this, GenerateKeyAndSendKeyActivity.class);
+        startActivity(intent);
+    }
+
+    private void openDecryptActivity() {
+        Intent intent = new Intent(this, DecryptActivity.class);
+        startActivity(intent);
+    }
+
+    private void openEncryptActivity() {
+        Intent intent = new Intent(this, EncryptActivity.class);
+        startActivity(intent);
+    }
+
+    private void openAboutAppActivity() {
+        Intent intent = new Intent(this, AboutAppActivity.class);
+        startActivity(intent);
     }
 }
