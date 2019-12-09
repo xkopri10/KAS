5118854e8b8b82bce89d25a150cda67ae8ba2b0e
xkopri10 <xkopri10@node.mendelu.cz>
2019-12-10 00:17:29 +0100

    email sending

--- app/src/main/java/com/example/kas_project/EncryptActivity.java
+++ app/src/main/java/com/example/kas_project/EncryptActivity.java
@@ -3,22 +3,56 @@ package com.example.kas_project;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.NestedScrollView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import java.util.Objects;
 
 public class EncryptActivity extends AppCompatActivity {
+
+    private EditText editTextTo;
+    private EditText messageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encrypt);
 
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         NestedScrollView view = (NestedScrollView) findViewById(R.id.nestedscrollviewEncrypt);
         view.setNestedScrollingEnabled(true);
 
-        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        editTextTo = findViewById(R.id.sendtoedittext);
+        messageEditText = findViewById(R.id.messageedittext);
+
+        Button sendButton = findViewById(R.id.sendbutton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmail();
+            }
+        });
+    }
+
+    private void sendEmail() {
+        String recipientList = editTextTo.getText().toString();
+        String[] recipients = recipientList.split(",");
+        String subject = "Encrypted message";
+        String message = messageEditText.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose an email client:"));
     }
 
     @Override
